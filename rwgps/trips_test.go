6f6ab0fee8244ab18d8a225d47bacb218d95d60f
Brian Zimmer <bzimmer@ziclix.com>
2022-09-06 08:55:13 +0200

    added pagination

--- rwgps/trips_test.go
+++ rwgps/trips_test.go
@@ -2,11 +2,13 @@ package rwgps_test
 
 import (
 	"context"
+	"encoding/json"
 	"net/http"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 
+	"github.com/bzimmer/activity"
 	"github.com/bzimmer/activity/rwgps"
 )
 
@@ -92,6 +94,81 @@ func TestRoute(t *testing.T) {
 			defer svr.Close()
 			trip, err := client.Trips.Route(context.TODO(), 94)
 			tt.after(trip, err)
+		})
+	}
+}
+
+func TestPagination(t *testing.T) {
+	t.Parallel()
+	a := assert.New(t)
+
+	tests := []struct {
+		name   string
+		trips  bool
+		routes bool
+		before func(mux *http.ServeMux)
+		after  func(trips []*rwgps.Trip, err error)
+	}{
+		{
+			name:   "valid routes",
+			routes: true,
+			before: func(mux *http.ServeMux) {
+				mux.HandleFunc("/users/88272/routes.json", func(w http.ResponseWriter, r *http.Request) {
+					enc := json.NewEncoder(w)
+					a.NoError(enc.Encode(struct {
+						Results []*rwgps.Trip `json:"results"`
+					}{
+						Results: []*rwgps.Trip{
+							{ID: 10},
+							{ID: 20},
+						},
+					}))
+				})
+			},
+			after: func(trips []*rwgps.Trip, err error) {
+				a.NoError(err)
+				a.NotNil(trips)
+				a.Len(trips, 2)
+			},
+		},
+		{
+			name:  "valid trips",
+			trips: true,
+			before: func(mux *http.ServeMux) {
+				mux.HandleFunc("/users/88272/trips.json", func(w http.ResponseWriter, r *http.Request) {
+					enc := json.NewEncoder(w)
+					a.NoError(enc.Encode(struct {
+						Results []*rwgps.Trip `json:"results"`
+					}{
+						Results: []*rwgps.Trip{
+							{ID: 110},
+							{ID: 210},
+						},
+					}))
+				})
+			},
+			after: func(trips []*rwgps.Trip, err error) {
+				a.NoError(err)
+				a.NotNil(trips)
+				a.Len(trips, 2)
+			},
+		},
+	}
+	for i := range tests {
+		tt := tests[i]
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			client, svr := newClient(tt.before)
+			defer svr.Close()
+			var err error
+			var trips []*rwgps.Trip
+			if tt.trips {
+				trips, err = client.Trips.Trips(context.TODO(), rwgps.UserID(88272), activity.Pagination{Total: 2})
+			}
+			if tt.routes {
+				trips, err = client.Trips.Routes(context.TODO(), rwgps.UserID(88272), activity.Pagination{Total: 2})
+			}
+			tt.after(trips, err)
 		})
 	}
 }
